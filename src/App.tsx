32af78e18da83bf4a430c4fe41654e7cec0184ef
Aymane Boukrouh <boukrouhaymane@gmail.com>
2023-02-13 01:18:05 +0100

    add option to add/remove nodes from grid

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,43 @@ function App() {
   const [gridX, setGridX] = useState<number>(0);
   const [gridY, setGridY] = useState<number>(0);
 
+  const onMouseEnter = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!e.currentTarget.classList.contains('has-node'))
+      e.currentTarget.classList.add('bg-light');
+  }
+
+  const onMouseLeave = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!e.currentTarget.classList.contains('has-node'))
+      e.currentTarget.classList.remove('bg-light');
+  }
+
+  const onMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.currentTarget.classList.contains('has-node')) {
+      e.currentTarget.innerHTML = '';
+      e.currentTarget.classList.remove('has-node');
+
+      return;
+    }
+
+    e.currentTarget.classList.add('has-node');
+    e.currentTarget.classList.remove('bg-light');
+
+    // add element to inside of cell
+    const cell = e.currentTarget;
+    const cellX = cell.getAttribute('data-x');
+    const cellY = cell.getAttribute('data-y');
+    const cellWidth = cell.clientWidth;
+    const cellHeight = cell.clientHeight;
+
+    const element = document.createElement('div');
+    element.classList.add('cell-node');
+    cell.appendChild(element);
+  }
+
   useEffect (() => {
-    setGridX(10);
-    setGridY(10);
-  }, [])
+    setGridX(20);
+    setGridY(35);
+  }, []);
 
   return (
     <div className="App">
@@ -18,7 +51,14 @@ function App() {
             return <div className="grid-row">
               {
                 Array.from(Array(gridY).keys()).map((y) => {
-                  return <div className="grid-cell"></div>
+                  return <div
+                    className     = "grid-cell position-relative"
+                    data-x        = {x}
+                    data-y        = {y}
+                    onMouseEnter  = {onMouseEnter}
+                    onMouseLeave  = {onMouseLeave}
+                    onMouseDown   = {onMouseDown}
+                  ></div>
                 })
               }
             </div>
