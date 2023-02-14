d65fbcceb2ebee9ed09c71694185d914d14a1798
Aymane Boukrouh <boukrouhaymane@gmail.com>
2023-02-14 21:56:02 +0100

    use bootstrap-icons

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'animejs';
 
 const root = ReactDOM.createRoot(
