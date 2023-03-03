b495499ce4f2b56e8c506ff3a67100566dc3fc36
Aymane Boukrouh <boukrouhaymane@gmail.com>
2023-03-03 23:12:52 +0100

    reset initial graph state

--- src/reducers/graphReducer.ts
+++ src/reducers/graphReducer.ts
@@ -1,39 +1,11 @@
 import { Graph, Node, Edge } from '@/types';
 
-/*const initialState = {
+const initialState = {
   graph: {
     nodes: [] as Node[],
     edges: [] as Edge[]
   } as Graph
-}*/
-// test
-const nodes = [
-  { label: 'A', coords: { x: 7*35, y: 5*35 } },
-  { label: 'B', coords: { x: 7*35, y: 10*35 } },
-  { label: 'C', coords: { x: 10*35, y: 10*35 } },
-  { label: 'D', coords: { x: 13*35, y: 10*35 } },
-  { label: 'E', coords: { x: 13*35, y: 5*35 } },
-  { label: 'F', coords: { x: 10*35, y: 5*35 } },
-]
-
-const edges = [
-  { source: nodes[0], target: nodes[1], weight: 3 },
-  { source: nodes[0], target: nodes[2], weight: 4 },
-  { source: nodes[0], target: nodes[5], weight: 7 },
-  { source: nodes[1], target: nodes[2], weight: 4 },
-  { source: nodes[2], target: nodes[3], weight: 9 },
-  { source: nodes[2], target: nodes[4], weight: 9 },
-  { source: nodes[2], target: nodes[5], weight: 1 },
-  { source: nodes[3], target: nodes[4], weight: 5 }
-]
-
-const initialState = {
-  graph: {
-    nodes: nodes as Node[],
-    edges: edges as Edge[]
-  } as Graph
 }
-// end test
 
 export const graphReducer = (state = initialState, action: any) => {
   switch (action.type) {
