f54a390f51831a58dcb934bf2efc55a4aef85f12
Aymane Boukrouh <boukrouhaymane@gmail.com>
2023-03-05 09:09:24 +0100

    refactor: wilcard import to avoid long line imports

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { graphReducer, modeReducer, mouseReducer, algorithmReducer, animationReducer, gridReducer } from '@/reducers';
+import * as reducers from '@/reducers';
 
 const store = configureStore({
   reducer: {
-    mode: modeReducer,
-    mouse: mouseReducer,
-    graph: graphReducer,
-    algorithm: algorithmReducer,
-    animation: animationReducer,
-    grid: gridReducer
+    mode: reducers.modeReducer,
+    mouse: reducers.mouseReducer,
+    graph: reducers.graphReducer,
+    algorithm: reducers.algorithmReducer,
+    animation: reducers.animationReducer,
+    grid: reducers.gridReducer,
   },
 });
 
